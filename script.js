92cfb8eac2291990d13e9214aeb3a680467bb5cc
Firefly08 <horearem@gmail.com>
2021-03-12 12:17:30 +0000

    Portfolio Read More

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function readmore() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("more");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
