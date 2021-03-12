e96cf31b35329c7a7704200add2199a00c5b3638
Firefly08 <horearem@gmail.com>
2021-03-12 12:59:27 +0000

    Contact

--- script.js
+++ script.js
@@ -1,5 +1,95 @@
-function readmore() {
-    var x = document.getElementById("more");
+function readmore1() {
+    var x = document.getElementById("more1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore2() {
+    var x = document.getElementById("more2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore3() {
+    var x = document.getElementById("more3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore4() {
+    var x = document.getElementById("more4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore5() {
+    var x = document.getElementById("more5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore6() {
+    var x = document.getElementById("more6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore7() {
+    var x = document.getElementById("more7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore8() {
+    var x = document.getElementById("more8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore9() {
+    var x = document.getElementById("more9");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore10() {
+    var x = document.getElementById("more10");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function readmore11() {
+    var x = document.getElementById("more11");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
