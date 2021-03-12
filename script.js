08b80b9e3be05d9d0cdc2f5d65d4bcd5c9a0cd8b
Firefly08 <horearem@gmail.com>
2021-03-12 09:26:49 +0000

    Menu, Banner, Skills

---
+++ script.js
@@ -0,0 +1,10 @@
+    // Add active class to the current link (highlight it)
+    var header = document.getElementById("menu");
+var links = header.getElementsByClassName("menu-link");
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
